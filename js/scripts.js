8fd982846018d70e9e839c273f7f01b70ad1f850
Elvandro <elvisamuni22@gmail.com>
2018-08-30 09:43:10 +0300

    Edited the scripts js file

--- js/scripts.js
+++ js/scripts.js
@@ -36,31 +36,6 @@
         }, 500);
     });
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
-
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
-
-    });
 
     // Open mobile menu
     $('#mobile-menu-open').click(function() {
@@ -71,13 +46,5 @@
     $('#mobile-menu-close').click(function() {
         $('header, body').removeClass('active');
     });
-
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
-
+    
 })(jQuery);
