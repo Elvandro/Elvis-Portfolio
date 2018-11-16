9bbf9212a7a398129854fb0eb229236f662df8d4
Elvandro <elvisamuni22@gmail.com>
2018-11-16 13:43:49 +0300

    Added scroll effects for the navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
-(function($) {
+$(document).ready(function() {
 
+    // Smooth scrolling
+    var scrollTab = $('.scroll');
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
-
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
-
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
-
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
+    scrollTab.click(function(e){
+      e.preventDefault();
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000);
     });
+
+    // Active link switching
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop();
+
+      scrollTab.each(function() {
+
+        var sectionOffset = $(this.hash).offset().top - 20;
+
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+        }
+      }
+    });
+
 
     // Scroll to top
     $('#to-top').click(function() {
@@ -36,15 +41,4 @@
         }, 500);
     });
 
-
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
-
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
-    
-})(jQuery);
+});
